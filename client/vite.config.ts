fec8d24189f2ae3d09d263cebb711b28020a55fe
wit <chaithongsorawit61@gmail.com>
2026-08-14 19:10:34 +0700

    feat: implement category API

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -19,3 +19,4 @@ export default defineConfig({
     include: ["tests/**/*.test.tsx"],
   },
 });
+
